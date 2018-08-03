dea6812a579eeaba8607349594a34150322923fb
Florian Chlan <florian@chlan.net>
2018-08-03 11:28:22 +0200

    Added config options for connecting GUI to chain

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  IPFS_GATEWAY_URL: 'http://localhost:8080/ipfs/',
+  KARA_CONTRACT_ADDRESS: ''
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  IPFS_GATEWAY_URL: '',
+  KARA_CONTRACT_ADDRESS: ''
 }
