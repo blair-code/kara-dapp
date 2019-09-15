5168c64b5b73f4ed5dc09ff26b5273ed00241cb2
David Dao <contact.daviddao@gmail.com>
2019-09-15 15:03:01 +0200

    Update public path

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/kara-dapp/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
